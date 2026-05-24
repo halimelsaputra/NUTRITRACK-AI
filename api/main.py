bd71b6f1ae6e5c8d4808a7a0fea885e26b767f67
Halim Elsa PUtra <163668075+halimelsaputra@users.noreply.github.com>
2026-05-24 22:01:32 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -198,6 +198,8 @@ SEGERA temui dokter spesialis — kondisi ini memerlukan penanganan medis segera
 # Path model
 BASE_DIR   = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, '..', 'model', 'nutritrack_model_gradienttape.keras')
+if not os.path.exists(MODEL_PATH):
+    MODEL_PATH = os.path.join(BASE_DIR, 'model', 'nutritrack_model_gradienttape.keras')
 
 
 # ============================================================
